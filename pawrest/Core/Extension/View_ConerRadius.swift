4c9121ec2e9ecd91c7b9428d355ad5c6489a6e36
soonny <134983918+soeun11@users.noreply.github.com>
2026-05-18 01:14:30 +0900

    [feat] tab bar 기본 구현

---
+++ pawrest/Core/Extension/View_ConerRadius.swift
@@ -0,0 +1,28 @@
+//
+//  View+CornerRadius.swift
+//  pawrest
+//
+//  Created by 소은 on 5/17/26.
+//
+
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        return Path(path.cgPath)
+    }
+}
