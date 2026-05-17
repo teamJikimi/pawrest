44d384f93aca7fac4ab3f7842a8ce284c83e3bd4
soonny <134983918+soeun11@users.noreply.github.com>
2026-05-18 01:14:30 +0900

    [feat] tab bar 기본 구현

--- pawrest/Features/Home/HomeView.swift
+++ pawrest/Features/Home/HomeView.swift
@@ -5,3 +5,44 @@
 //  Created by 소은 on 5/17/26.
 //
 
+import SwiftUI
+import ComposableArchitecture
+
+struct HomeView: View {
+    let store: StoreOf<HomeFeature>
+    
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 20) {
+                Text(store.text)
+                    .font(.title)
+                
+                Text("카운트: \(store.count)")
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                
+                HStack(spacing: 20) {
+                    Button {
+                        store.send(.decrementButtonTapped)
+                    } label: {
+                        Image(systemName: "minus.circle.fill")
+                            .font(.largeTitle)
+                    }
+                    
+                    Button {
+                        store.send(.incrementButtonTapped)
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .font(.largeTitle)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView(store: Store(initialState: HomeFeature.State()) {
+        HomeFeature()
+    })
+}
