3a1676bef995a8fecba15a029bbe262675f374b6
mxxn.4y <mxxn.4y@gmail.com>
2026-05-28 10:55:43 +0900

    [setting] Community 댓글 행 컴포넌트 기본 구조

---
+++ pawrest/Core/Components/Community/CommunityCommentRow.swift
@@ -0,0 +1,26 @@
+//
+//  CommunityCommentRow.swift
+//  pawrest
+//
+//  Created by Moon AYoung on 5/28/26.
+//
+
+import SwiftUI
+
+struct CommunityCommentRow: View {
+    
+    //MARK: - Properties
+    
+    let comment: Comment
+    var isReply: Bool
+    
+    //MARK: - Body
+    var body: some View {
+    }
+}
+
+//MARK: - Subviews
+
+private extension CommunityCommentRow {
+    
+}
