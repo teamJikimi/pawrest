d2eddf691f99ff4b965bad87e777e8fa57c7f45a
mxxn.4y <mxxn.4y@gmail.com>
2026-05-29 17:35:55 +0900

    [setting] CommunityCommentInputBar 기본 파일

---
+++ pawrest/Core/Components/Community/CommunityCommentInputBar.swift
@@ -0,0 +1,22 @@
+//
+//  CommunityCommentInputBar.swift
+//  pawrest
+//
+//  Created by Moon AYoung on 5/29/26.
+//
+
+import SwiftUI
+
+struct CommunityCommentInputBar: View {
+    
+    //MARK: - Properties
+    
+    @Binding var text: String
+    var placeholder: String = " "
+    let onSend: () -> Void
+    
+    var body: some View {
+        
+    }
+    
+}
