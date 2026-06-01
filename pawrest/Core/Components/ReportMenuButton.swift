9fa38fdf809ada28ff8b383f216b61619e4b1af9
mxxn.4y <mxxn.4y@gmail.com>
2026-06-02 02:41:54 +0900

    [fix] ReportMenuButton conflicts

--- pawrest/Core/Components/ReportMenuButton.swift
+++ pawrest/Core/Components/ReportMenuButton.swift
@@ -144,7 +144,7 @@ struct ReportMenuButton: View {
                 }
                 .frame(width: 160)
                 .background(Color.white)
-                .cornerRadius(10)
+                .cornerRadius(10, corners: .allCorners)
                 .overlay(
                     RoundedRectangle(cornerRadius: 10)
                         .stroke(.gray20, lineWidth: 1)
