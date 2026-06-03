1fd58a96cbf5b926c67ae493a7d82f35f7c060f7
mxxn.4y <mxxn.4y@gmail.com>
2026-06-03 19:59:40 +0900

    [feat] 커뮤니티 메인피드 검색창, 정렬 드롭다운 추가

--- pawrest/Core/Components/Community/CommunitySearchBar.swift
+++ pawrest/Core/Components/Community/CommunitySearchBar.swift
@@ -1,7 +1,43 @@
 //
-//  C.swift
+//  CommunitySearchBar.swift
 //  pawrest
 //
 //  Created by Moon AYoung on 6/3/26.
 //
 
+import SwiftUI
+
+struct CommunitySearchBar: View {
+    
+    //MARK: - Properties
+    
+    @Binding var text: String
+    var placeholder: String = "제목, 내용으로 검색..."
+    @FocusState.Binding var isFocused: Bool
+    
+    //MARK: - Body
+    
+    var body: some View {
+        HStack(spacing: 6) {
+            Image(systemName: "magnifyingglass")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 16, height: 16)
+                .foregroundColor(.gray60)
+            
+            TextField(
+                "",
+                text: $text,
+                prompt: Text(placeholder).foregroundColor(.gray50)
+            )
+            .typography(.body2R1)
+            .foregroundColor(.gray80)
+            .focused($isFocused)
+        }
+        .padding(.horizontal, 12)
+        .padding(.vertical, 9)
+        .frame(maxWidth: .infinity)
+        .background(.gray10)
+        .cornerRadius(10, corners: .allCorners)
+    }
+}
