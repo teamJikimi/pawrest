9aba0e0d4db23a07fa765d139b5fd70b576ccb66
mxxn.4y <mxxn.4y@gmail.com>
2026-06-18 01:48:41 +0900

    [feat] 커스텀 네비바에 alert 연결

--- pawrest/Features/Scene/Home/SelfAssessmentItem/SelfAssessmentView.swift
+++ pawrest/Features/Scene/Home/SelfAssessmentItem/SelfAssessmentView.swift
@@ -28,19 +28,7 @@ struct SelfAssessmentView: View {
                 .padding(.horizontal, 20)
             }
             .background(.gray10)
-            
-            //            .onChange(of: store.firstUnansweredIndex) { _, _ in
-            //
-            //            }
-            //            .onReceive(
-            //                NotificationCenter.default.publisher(for: .init("scrollToUnanswered"))
-            //            ) { _ in
-            //                if let index = store.firstUnansweredIndex {
-            //                    withAnimation {
-            //                        proxy.scrollTo("question_\(index)", anchor: .top)
-            //                    }
-            //                }
-            //            }
+
             .onChange(of: store.scrollTargetIndex) { _, targetIndex in
                 if let targetIndex {
                     withAnimation {
@@ -54,10 +42,24 @@ struct SelfAssessmentView: View {
         .customNavigationBar(
             store: store.scope(state: \.navigationBar, action: \.navigationBar)
         )
-        
+        .overlay(alignment: .topLeading) {
+            Button {
+                store.send(.backButtonTapped)
+            } label: {
+                Image(.iconBack)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 24, height: 24)
+                    .foregroundStyle(.gray90)
+            }
+            .frame(width: 44, height: 44)
+            .padding(.leading, 10)
+        }
+
         .onChange(of: store.shouldDismiss) { _, shouldDismiss in
             if shouldDismiss { dismiss() }
         }
+        
         .alert(
             "검사를 종료할까요?",
             isPresented: Binding(
